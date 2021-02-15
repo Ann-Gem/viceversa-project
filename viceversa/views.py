b534e18739c4cac4b75358103ab05e23a27098d8
Ann_Gem <ahiotka.d@gmail.com>
2021-02-15 16:23:30 +0300

    Add words count feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home.html")
 
-
+count = 0
 def reverse(request):
+
 	user_text = request.GET['usertext']
+	n = user_text.count(' ') + 1
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext': reversed_text})
+	return render(request, 'reverse.html' , {'usertext':user_text, 'reversedtext': reversed_text, 'number_of_words':n})
